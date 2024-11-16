805aed4eaf1781df4b36911c253e2876865ad3f8
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-11-16 13:40:12 +1300

    style: Added coinbase logo to wallet connect.

--- components/custom/CoinbaseWalletLogo.tsx
+++ components/custom/CoinbaseWalletLogo.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export const CoinbaseWalletLogo: React.FC = () => (
-  <div
+  <img
+    src="../../public/images/coinbase.png"
+    alt="Coinbase Wallet Logo"
     style={{
       width: 24,
       height: 24,
-      backgroundColor: 'white',
       borderRadius: '50%',
+      objectFit: 'cover',
     }}
   />
 );
