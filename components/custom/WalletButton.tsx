edc8df4cd83e77498a69f8102dc15e98ec4fd339
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-11-16 15:56:18 +1300

    feat: Implemented spending perm logic into button

--- components/custom/WalletButton.tsx
+++ components/custom/WalletButton.tsx
@@ -1,5 +1,18 @@
+'use client';
+
+import { cn, color, pressable, text } from '@coinbase/onchainkit/theme';
+import { Address, Hex, parseUnits } from 'viem';
+import { useQuery } from '@tanstack/react-query';
+import { spendPermissionManagerAddress } from '@/lib/abi/SpendPermissionManager';
+
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { useConnect } from 'wagmi';
+import {
+  useAccount,
+  useChainId,
+  useConnect,
+  useConnectors,
+  useSignTypedData,
+} from 'wagmi';
 import { CoinbaseWalletLogo } from './CoinbaseWalletLogo';
 
 const GRADIENT_BORDER_WIDTH = 2;
@@ -61,6 +74,23 @@ function Gradient({
 }
 
 export function WalletButton({ height = 66, width = 200 }) {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [signature, setSignature] = useState<Hex>();
+  const [transactions, setTransactions] = useState<Hex[]>([]);
+  const [spendPermission, setSpendPermission] = useState<object>();
+
+  const { signTypedDataAsync } = useSignTypedData();
+  const account = useAccount();
+  const chainId = useChainId();
+  const { connectAsync } = useConnect();
+
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ['collectSubscription'],
+    queryFn: handleCollectSubscription,
+    refetchOnWindowFocus: false,
+    enabled: !!signature,
+  });
+
   const { connectors, connect } = useConnect();
 
   const minButtonHeight = 48;
@@ -118,16 +148,159 @@ export function WalletButton({ height = 66, width = 200 }) {
     }
   }, [connectors, connect]);
 
+  async function handleSubmit() {
+    setIsDisabled(true);
+    let accountAddress = account?.address;
+    if (!accountAddress) {
+      try {
+        const requestAccounts = await connectAsync({
+          connector: connectors[0],
+        });
+        accountAddress = requestAccounts.accounts[0];
+      } catch {
+        return;
+      }
+    }
+
+    const spendPermission = {
+      account: accountAddress, // User wallet address
+      spender: process.env.NEXT_PUBLIC_SPENDER_ADDRESS! as Address, // Spender smart contract wallet address
+      token: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' as Address, // ETH (https://eips.ethereum.org/EIPS/eip-7528)
+      allowance: parseUnits('10', 18),
+      period: 86400, // seconds in a day
+      start: 0, // unix timestamp
+      end: 281474976710655, // max uint48
+      salt: BigInt(0),
+      extraData: '0x' as Hex,
+    };
+
+    try {
+      const signature = await signTypedDataAsync({
+        domain: {
+          name: 'Spend Permission Manager',
+          version: '1',
+          chainId: chainId,
+          verifyingContract: spendPermissionManagerAddress,
+        },
+        types: {
+          SpendPermission: [
+            { name: 'account', type: 'address' },
+            { name: 'spender', type: 'address' },
+            { name: 'token', type: 'address' },
+            { name: 'allowance', type: 'uint160' },
+            { name: 'period', type: 'uint48' },
+            { name: 'start', type: 'uint48' },
+            { name: 'end', type: 'uint48' },
+            { name: 'salt', type: 'uint256' },
+            { name: 'extraData', type: 'bytes' },
+          ],
+        },
+        primaryType: 'SpendPermission',
+        message: spendPermission,
+      });
+      setSpendPermission(spendPermission);
+      setSignature(signature);
+    } catch (e) {
+      console.error(e);
+    }
+    setIsDisabled(false);
+  }
+
+  async function handleCollectSubscription() {
+    setIsDisabled(true);
+    let data;
+    try {
+      const replacer = (key: string, value: any) => {
+        if (typeof value === 'bigint') {
+          return value.toString();
+        }
+        return value;
+      };
+      const response = await fetch('/collect', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+          {
+            spendPermission,
+            signature,
+            dummyData: Math.ceil(Math.random() * 100),
+          },
+          replacer
+        ),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      data = await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+    setIsDisabled(false);
+    return data;
+  }
+
+  useEffect(() => {
+    if (!data) return;
+    setTransactions([data?.transactionHash, ...transactions]);
+  }, [data]);
+
   return (
-    <button style={buttonStyles} onClick={createWallet}>
-      <div style={styles.gradientContainer}>
-        <Gradient style={styles.gradient}>
-          <div style={styles.buttonBody}>
-            <CoinbaseWalletLogo />
-            Create Wallet
+    <div>
+      {!signature ? (
+        <div className="flex w-[450px]">
+          <button
+            style={buttonStyles}
+            onClick={handleSubmit}
+            type="button"
+            disabled={isDisabled}
+            data-testid="ockTransactionButton_Button"
+          >
+            <div style={styles.gradientContainer}>
+              <Gradient style={styles.gradient}>
+                <div style={styles.buttonBody}>
+                  <CoinbaseWalletLogo />
+                  Subscribe
+                </div>
+              </Gradient>
+            </div>
+          </button>
+        </div>
+      ) : (
+        <div className="space-y-8 w-[450px]">
+          <div className="flex">
+            <button
+              style={buttonStyles}
+              onClick={() => refetch()}
+              type="button"
+              disabled={isDisabled}
+              data-testid="collectSubscriptionButton_Button"
+            >
+              <div style={styles.gradientContainer}>
+                <Gradient style={styles.gradient}>
+                  <div style={styles.buttonBody}>Collect Subscription</div>
+                </Gradient>
+              </div>
+            </button>
           </div>
-        </Gradient>
-      </div>
-    </button>
+          <div className="h-80 space-y-4 relative">
+            <div className="text-lg font-bold">Subscription Payments</div>
+            <div className="flex flex-col">
+              {transactions.map((transactionHash, i) => (
+                <a
+                  key={i}
+                  className="hover:underline text-ellipsis truncate"
+                  target="_blank"
+                  href={`https://sepolia.basescan.org/tx/${transactionHash}`}
+                >
+                  View transaction {transactionHash}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
