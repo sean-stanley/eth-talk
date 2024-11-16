3d8b82f27b66637e8e2a9181deb4b72169e26e82
Sean Stanley <sean@redheadweb.nz>
2024-11-16 22:06:44 +0700

    fix: webhook reply. pass stream from our openai reply back to Omi server

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,38 +1,14 @@
+// import { default as stream } from 'node:stream';
+import type { ReadableStream } from 'node:stream/web';
 import { openai } from '@ai-sdk/openai';
 import { generateText } from 'ai';
 
 import { generateUUID } from '@/lib/utils';
 
-const START_TRIGGER_PHRASE = [
-  'hey smart wallet',
-  'hey, mart wallet',
-  'hey matts wallet,',
-  'hey smot wallet',
-];
-
-const STOP_TRIGGER_PHRASE = [
-  'thanks smart wallet',
-  'thanks smot wallet',
-  'thanks mart wallet',
-  'thanks matts wallet',
-];
-
 export async function POST(request: Request) {
   const body = await request.json();
   const userId = new URL(request.url).searchParams.get('uid');
   console.log(userId, body);
-
-  // Keywords
-  const cryptoKeywords = [
-    'crypto',
-    'blockchain',
-    'Bitcoin',
-    'Ethereum',
-    'wallet',
-    'swap',
-    'transaction',
-    'portfolio',
-  ];
 
   // Check the actionItems field for relevant keywords
   const transcriptSegments = body.transcript_segments
@@ -47,7 +23,7 @@ export async function POST(request: Request) {
       'determine if the conversation is asking for AI assistance to interact with their smart wallet. Return a sanitised command wrapped with "hey smart wallet" and "thanks smart wallet" triggers. Requests can be to transfer, swap, check balance, check their network, check wallet updates, check prices, get an ens, etc. When the text is not a command, return nothing.',
     prompt: transcriptSegments,
   });
-  console.log('text response:', textResponse);
+
   if (textResponse.text) {
     const streamedResponse = await fetch(
       'https://nextjs-ai-chatbot-ebon-iota.vercel.app/api/chat',
@@ -64,12 +40,10 @@ export async function POST(request: Request) {
       }
     );
 
-    const result = await streamedResponse.json();
-    console.log(result);
-
-    return new Response(JSON.stringify(streamedResponse), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+    if (streamedResponse.body) {
+      // stream response back to Omi app.
+      return streamedResponse
+    }
   }
 
   return new Response(
