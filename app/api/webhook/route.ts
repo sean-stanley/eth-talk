295d53cb99bc71a87f2bee831d78b676bb284527
Sean Stanley <sean@redheadweb.nz>
2024-11-16 19:54:53 +0700

    fix: transcript json

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,6 @@
+import { generateText } from 'ai';
+import { openai } from '@ai-sdk/openai';
+
 const START_TRIGGER_PHRASE = [
   'hey smart wallet',
   'hey, mart wallet',
@@ -28,31 +31,45 @@ export async function POST(request: Request) {
   ];
 
   // Check the actionItems field for relevant keywords
-  const relevantActionItems = body.structured.actionItems.filter(
-    (action: string) =>
-      cryptoKeywords.some((keyword) =>
-        action.toLowerCase().includes(keyword.toLowerCase())
-      )
-  );
+  const transcriptSegments = body.structured.transcript_segments.map((s) => s.text).join("\n");
 
-  console.log('Relevant Crypto Action Items:', relevantActionItems);
+  console.log('transcribed text for the memory:', transcriptSegments);
 
-  let streamedResponse = await fetch('/api/chat', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      id: body.id,
-      messages: body.messages,
-      modelId: body.modelId,
+  const textResponse = await generateText({
+    model: openai.languageModel('gpt-4o-mini'),
+    system:
+      'determine if the conversation is asking for AI assistance to interact with their smart wallet. Return a sanitised command wrapped with "hey smart wallet" and "thanks smart wallet" triggers. Requests can be to transfer, swap, check balance, check their network, check wallet updates, check prices, get an ens, etc. When the text is not a command, return nothing.',
+    prompt: transcriptSegments,
+  });
+  console.log('text response:', textResponse);
+  if (textResponse.text) {
+    const streamedResponse = await fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: body.id,
+        messages: [{ role: 'user', content: textResponse.text }],
+        modelId: body.modelId,
+      }),
+    });
+
+      const result = await streamedResponse.json();
+      console.log(result);
+
+      return new Response(JSON.stringify(streamedResponse), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+  }
+
+  return new Response(
+    JSON.stringify({
+      role: 'agent',
+      content: 'No crypto commands to respond to',
     }),
-  });
-
-  const result = await streamedResponse.json();
-  console.log(result);
-
-  return new Response(JSON.stringify(streamedResponse), {
-    headers: { 'Content-Type': 'application/json' },
-  });
+    {
+      headers: { 'Content-Type': 'application/json' },
+    }
+  );
 }
